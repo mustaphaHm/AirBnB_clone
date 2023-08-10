4d29438ecf58ef61c924d6d09391f0d473717adf
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-10 05:58:28 -0700

    Tests for base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Implementing UnitTests"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_initialization(self):
+        instance = BaseModel()
+        """Test if Id is Valid UUID"""
+        self.assertTrue(instance.id)
+        self.assertIsInstance(instance.id, str)
+        self.assertRegex(instance.id, r'^[0-9a-f]{8}-[0-9a-f]{4}\
+                -[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$')
+        """Test if created_at and updated_at are instances of datetime"""
+        self.assertIsInstance(instance.created_at, datetime)
+        self.assertIsInstance(instance.updated_at, datetime)
+
+    def test_save_method(self):
+        instance = BaseModel()
+        """Test if updated_at get updated"""
+        init_updated_at = instance.updated_at
+        instance.save()
+        new_updated_at = instance.updated_at
+        self.assertNotEqual(init_updated_at, new_updated_at)
+
+    def test_to_dict_method(self):
+        instance = BaseModel()
+        """Test the function to dict"""
+        instance_dict = instance.to_dict()
+        self.assertIsInstance(instance_dict, dict)
+        self.assertIn('__class__', instance_dict)
+        self.assertIn('id', instance_dict)
+        self.assertIn('created_at', instance_dict)
+        self.assertIn('updated_at', instance_dict)
+        self.assertEqual(instance_dict['__class__'], 'BaseModel')
+
+    if __name__ == "__main__":
+        unittest.main()
