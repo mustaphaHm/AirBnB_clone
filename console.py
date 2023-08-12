c0223fbd73cbd37afd7e80a69a5b61b2a1b8603e
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-12 09:46:53 -0700

    Task 7 , modifiy the console.py file

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""This is console mudole."""
+"""This is console module."""
 import cmd
+import models
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Definition of class HBNBCommand."""
 
     prompt = "(hbnb) "
+    classes = {
+        'BaseModel': BaseModel
+    }
 
     def do_quit(self, line):
         """Quit to exit the program."""
@@ -19,6 +24,96 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when it's an empty line."""
         pass
+
+    def validate_class_and_id(self, line):
+        """Validate class name and instance ID."""
+        if not line:
+            print("** class name missing **")
+            return None, None
+
+        args = line.split()
+        class_name = args[0]
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return None, None
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return None, None
+
+        obj_dict = models.storage.all()
+        key = class_name + "." + args[1]
+        if key not in obj_dict:
+            print("** no instance found **")
+            return None, None
+
+        return class_name, key
+
+    def do_create(self, line):
+        """Create a new instance of BaseModel."""
+        if not line:
+            print("** class name missing **")
+            return
+        class_name = line.split()[0]
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[class_name]()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, line):
+        """Print the string representation of an instance."""
+        class_name, key = self.validate_class_and_id(line)
+        if class_name and key:
+            print(models.storage.all()[key])
+
+    def do_destroy(self, line):
+        """Delete an instance by id."""
+        class_name, key = self.validate_class_and_id(line)
+        if class_name and key:
+            del models.storage.all()[key]
+            models.storage.save()
+
+    def do_all(self, line):
+        """Print all string representation of all instances."""
+        obj_list = []
+        if not line:
+            for key, obj in models.storage.all().items():
+                obj_list.append(str(obj))
+            print(obj_list)
+        else:
+            class_name = line.split()[0]
+            if class_name not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for key, obj in models.storage.all().items():
+                if class_name in key:
+                    obj_list.append(str(obj))
+            print(obj_list)
+
+    def do_update(self, line):
+        """Update an instance based on the class name and id."""
+        class_name, key = self.validate_class_and_id(line)
+        if class_name and key:
+            args = line.split()
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        attr_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attr_value = args[3]
+        obj_dict = models.storage.all()
+        instance = obj_dict[key]
+        try:
+            # Try to cast the attribute value to the attribute type
+            attr_type = type(getattr(instance, attr_name))
+            setattr(instance, attr_name, attr_type(attr_value))
+            instance.save()
+        except (ValueError, TypeError):
+            pass
 
 
 if __name__ == '__main__':
